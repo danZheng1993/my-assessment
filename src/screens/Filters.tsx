39e0e77f749605d4d69bcdea2ae4299943863714
Daniel Zheng <dannyzheng1993@gmail.com>
2023-01-11 20:33:32 -0700

    Year filter update

--- src/screens/Filters.tsx
+++ src/screens/Filters.tsx
@@ -1,18 +1,33 @@
-import React, { useCallback, useState } from "react";
-import { StyleSheet, TextInput, View } from "react-native";
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
+import Slider from "@react-native-community/slider";
+import RadioGroup from "react-native-radio-buttons-group";
+import type { RadioButtonProps } from "react-native-radio-buttons-group";
 
-import { FILTER_TYPE, ROOT_STACK_PARAM_LIST } from "../constants/dataTypes";
+import {
+  FILTER_TYPE,
+  ROOT_STACK_PARAM_LIST,
+  YEAR_FILTER_OPTION,
+} from "../constants/dataTypes";
 import { FiltersList } from "../constants/suggestions";
 import Button from "../components/Button";
-import { useAppDispatch } from "../hooks/reducer";
-import { fetchCars } from "../reducer/cars";
+import { useAppDispatch, useAppSelector } from "../hooks/reducer";
+import { fetchCars, selectFilter } from "../reducer/cars";
+import Typography from "../components/Typography";
 
 type Props = NativeStackScreenProps<ROOT_STACK_PARAM_LIST, "Filters">;
 
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
+    padding: 16,
   },
   suggestionsWrapper: {
     flex: 1,
@@ -31,17 +46,90 @@ const styles = StyleSheet.create({
     marginRight: 8,
     paddingHorizontal: 8,
     paddingVertical: 8,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
     borderBottomWidth: 1,
     borderBottomColor: "#1C1C1C",
   },
+  clearButton: {
+    width: 24,
+    height: 24,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  valueIndicator: {
+    textAlign: "center",
+  },
+  options: {
+    alignSelf: "center",
+    alignItems: "flex-start",
+  },
 });
+
+const YEAR_OPTION_MAP: { [key: string]: YEAR_FILTER_OPTION } = {
+  lt: YEAR_FILTER_OPTION.LT,
+  gt: YEAR_FILTER_OPTION.GT,
+  exact: YEAR_FILTER_OPTION.EXACT,
+};
 
 const Filters: React.FC<Props> = ({ route, navigation }) => {
   const dispatch = useAppDispatch();
   const filterType = route.params?.filterType;
-  const filter = FiltersList.find((filter) => filter.type === filterType);
+  const filterOption = FiltersList.find((filter) => filter.type === filterType);
+  const selectedFilter = useAppSelector(selectFilter);
 
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState(
+    filterType === FILTER_TYPE.YEAR ? `${filterOption?.min}` : ""
+  );
+
+  useEffect(() => {
+    if (selectedFilter?.type === filterType) {
+      setValue(selectedFilter.value);
+      if (filterType === FILTER_TYPE.YEAR) {
+        setYearOptions([
+          {
+            id: "1",
+            value: "exact",
+            label: "Exact year",
+            selected: selectedFilter?.option === YEAR_FILTER_OPTION.EXACT,
+          },
+          {
+            id: "2",
+            value: "gt",
+            label: "Greater than",
+            selected: selectedFilter?.option === YEAR_FILTER_OPTION.GT,
+          },
+          {
+            id: "3",
+            value: "lt",
+            label: "Less than",
+            selected: selectedFilter?.option === YEAR_FILTER_OPTION.LT,
+          },
+        ]);
+      }
+    }
+  }, [selectedFilter, filterType]);
+
+  const [yearOptions, setYearOptions] = useState<RadioButtonProps[]>([
+    {
+      id: "1",
+      value: "exact",
+      label: "Exact year",
+      selected: true,
+    },
+    {
+      id: "2",
+      value: "gt",
+      label: "Greater than",
+    },
+    {
+      id: "3",
+      value: "lt",
+      label: "Less than",
+    },
+  ]);
 
   const handleSuggestion = useCallback(
     (suggestion: string) => {
@@ -59,31 +147,54 @@ const Filters: React.FC<Props> = ({ route, navigation }) => {
   const handleFilter = useCallback(() => {
     if (value) {
       dispatch(
-        fetchCars({
-          type: filterType,
-          value,
-        })
+        fetchCars(
+          filterType === FILTER_TYPE.YEAR
+            ? {
+                type: filterType,
+                value,
+                option:
+                  YEAR_OPTION_MAP[
+                    yearOptions?.find((option) => option.selected)?.value ??
+                      "exact"
+                  ],
+              }
+            : {
+                type: filterType,
+                value,
+              }
+        )
       );
       navigation.goBack();
     }
-  }, [value, filterType]);
+  }, [value, filterType, yearOptions]);
+
+  const handleOptionChange = (options: RadioButtonProps[]) => {
+    setYearOptions(options);
+  };
+
+  const handleClear = () => setValue("");
 
   if (filterType !== FILTER_TYPE.YEAR) {
     const renderingSuggestion =
       value.length === 0
-        ? filter?.suggestion
-        : filter?.suggestion?.filter((suggestion) =>
+        ? filterOption?.suggestion
+        : filterOption?.suggestion?.filter((suggestion) =>
             suggestion.startsWith(value)
           );
     return (
       <View style={styles.wrapper}>
         <View style={styles.inputWrapper}>
-          <TextInput
-            style={styles.input}
-            value={value}
-            onChangeText={setValue}
-            autoFocus
-          />
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              value={value}
+              onChangeText={setValue}
+              autoFocus
+            />
+            <TouchableOpacity style={styles.clearButton} onPress={handleClear}>
+              <Text>X</Text>
+            </TouchableOpacity>
+          </View>
           <Button
             variant="secondary"
             title="Apply Filter"
@@ -94,7 +205,7 @@ const Filters: React.FC<Props> = ({ route, navigation }) => {
           {renderingSuggestion?.map((suggestion, idx) => (
             <Button
               title={suggestion}
-              variant="primary"
+              variant={suggestion === value ? "secondary" : "primary"}
               onPress={() => handleSuggestion(suggestion)}
               key={`suggestion_${idx}`}
             />
@@ -103,7 +214,28 @@ const Filters: React.FC<Props> = ({ route, navigation }) => {
       </View>
     );
   }
-  return <View style={styles.wrapper} />;
+
+  return (
+    <View style={styles.wrapper}>
+      <Typography variant="subtitle">Please select a model year</Typography>
+      <Slider
+        minimumValue={filterOption?.min ?? 1900}
+        maximumValue={filterOption?.max ?? new Date().getFullYear()}
+        step={1}
+        onValueChange={(value) => setValue(`${value}`)}
+        value={Number(value)}
+      />
+      <Typography variant="body" style={styles.valueIndicator}>
+        {value}
+      </Typography>
+      <RadioGroup
+        radioButtons={yearOptions}
+        onPress={handleOptionChange}
+        containerStyle={styles.options}
+      />
+      <Button variant="secondary" title="Apply Filter" onPress={handleFilter} />
+    </View>
+  );
 };
 
 export default Filters;
